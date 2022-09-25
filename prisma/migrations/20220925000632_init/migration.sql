853e5d1df3da4be3fdb7b74a143e817cd8c6a157
Jeferson Ferreira <fjeferson60@gmail.com>
2022-09-24 22:28:29 -0300

    Create tables

--- prisma/migrations/20220925000632_init/migration.sql
+++ prisma/migrations/20220925000632_init/migration.sql
@@ -1,15 +1,9 @@
--- CreateEnum
-CREATE TYPE "ShoeSize" AS ENUM ('BR_31', 'BR_40', 'BR_41', 'BR_43');
-
--- CreateEnum
-CREATE TYPE "ClothingSize" AS ENUM ('PP', 'P', 'M', 'G', 'GG', 'XG');
-
 -- CreateTable
 CREATE TABLE "Product" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "category" TEXT NOT NULL,
-    "size" "ShoeSize",
+    "size" TEXT NOT NULL,
     "price" INTEGER NOT NULL,
     "percent" INTEGER NOT NULL,
     "discount" BOOLEAN NOT NULL,
--- src/controller/products.js
+++ src/controller/products.js
@@ -1,6 +1,9 @@
+import { list } from '../repositories/product.js';
 import { createProduct } from '../service/products/create.js';
+
 export async function listAll(req, res) {
-  return res.json({ id: 1, name: 'Sapato' });
+  const response = await list();
+  return res.status(200).json(response);
 }
 
 export async function create(req, res) {
